6fb394610e6c17d1db879fb1e944b4b02a174e06
bigwood-property <bigwood.property@gmail.com>
2026-05-06 20:08:09 +0100

    Update skin.js

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.1/17227
 // Filename: bigwood_v1.ggsk
-// Generated Wed May 6 20:05:28 2026
+// Generated Wed May 6 20:07:59 2026
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
